0c651c773609631b0b21037ad9a119e34812bbbe
Max <108481466+Max-js@users.noreply.github.com>
2025-01-13 20:25:30 -0800

    feature/add-user-input-ability (#2)
    * add cl user interface, adjust formatting throughout
    
    * adjust input toLowerCase function
    
    * remove space
    
    * adjust naming convention

--- app.js
+++ app.js
@@ -1,68 +1,99 @@
+//Suppress error for tough-cookie dependancy deprecation
+process.noDeprecation = true;
 require('dotenv').config();
+const readline = require('node:readline');
 const request = require('request');
 //When running without env file, replace domain with url, excluding https:// and any trailing /'s
 const domain = process.env.DOMAIN;
 //When running without env file replace admin_token with token string
 const admin_token = process.env.ADMIN_TOKEN;
 
-const queryString = `
-  query {
-    products(first: 10) {
-      edges {
-        node {
-          id
-          title
-          variants(first: 20) {
-            edges {
-              node {
-                id
-                title
-                price
+function getProducts(productName) {
+  const query = `
+    query {
+      products(first: 10${productName ? `, query: "title:${productName}"` : ''}) {
+        edges {
+          node {
+            id
+            title
+            variants(first: 20) {
+              edges {
+                node {
+                  id
+                  title
+                  price
+                }
               }
             }
           }
         }
       }
     }
-  }
-`;
+  `;
 
-function getProducts() {
-    const options = {
-      url: `https://${domain}/admin/api/2025-01/graphql.json`,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': admin_token
-      },
-      body: JSON.stringify({
-        query: queryString,
-      })
-    };
-    request(options, (error, body) => {
-        if (error) {
-          console.error('Error fetching products:', error);
-          return;
-        }
+  const options = {
+    url: `https://${domain}/admin/api/2025-01/graphql.json`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': admin_token
+    },
+    body: JSON.stringify({
+      query: query,
+    })
+  };
 
-        const result = JSON.parse(body.body);
-
-        if (result.errors) {
-          console.error('GraphQL Errors:', result.errors);
-        } else {
-          const products = result.data.products.edges.map(edge => {
-            const product = edge.node;
-            const variants = product.variants.edges.map(variantEdge => variantEdge.node);
-            variants.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-            return {
-              ...product,
-              variants
-            };
-          });
-
-          console.log('Retrieved Products and Variants:', JSON.stringify(products, null, 2));
-        }
-      });
+  request(options, (error, body) => {
+    if (error) {
+      console.error('Error fetching products:', error);
+      return;
     }
 
-getProducts();
+    const result = JSON.parse(body.body);
+
+    if (result.errors) {
+      console.error('GraphQL Errors:', result.errors);
+    } else {
+      const products = result.data.products.edges.map(edge => {
+        const product = edge.node;
+        const variants = product.variants.edges.map(variantEdge => variantEdge.node);
+        variants.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+        return {
+          ...product,
+          variants
+        };
+      });
+
+      console.log('Retrieved Products and Variants:', JSON.stringify(products, null, 2));
+      askSearchAgain();
+    }
+  });
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to the Shopify product search tool!');
+
+function askSearchAgain() {
+  rl.question('Would you like to search another product? (y/n): ', input => {
+    if (input.toLowerCase() === 'y') {
+      runSearch();
+    } else if (input.toLowerCase() === 'n') {
+      rl.close();
+    } else {
+      console.log('Invalid input, please enter "y" or "n".');
+      askSearchAgain(); 
+    }
+  })
+}
+
+function runSearch() {
+  rl.question('Enter a product name to search or leave this field blank if you want to display all products: ', productName => {
+    getProducts(productName);
+  })
+}
+
+runSearch();
